5a6f3f8a3f66901a47a9d42f40f2b7aae4633d5e
Julia <785533217@qq.com>
2020-11-04 21:53:08 +0800

    搭建骨架- 每个页面+设置路由

--- src/backend.py
+++ src/backend.py
@@ -3,16 +3,43 @@
 
 # 导入Flask类
 from flask import Flask
+from flask_restful import Api, Resource
 # 实例化，可视为固定格式
 app = Flask(__name__)
+api = Api(app)
 
-# route()方法用于设定路由；类似spring路由配置
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# 首页
+class Main(Resource):
+    def get(self):
+        return {'hello': 'Main'}
+
+# 用户管理
+class UserApi(Resource):
+    def get(self):
+        return {'hello': 'UserApi'}
+
+# 用例管理
+class TestcaseApi(Resource):
+    def get(self):
+        return {'hello': 'TestcaseApi'}
+
+# 任务管理
+class TaskApi(Resource):
+    def get(self):
+        return {'hello': 'TaskApi'}
+
+# 报告管理
+class ReportApi(Resource):
+    def get(self):
+        return {'hello': 'ReportApi'}
+
+# 添加路由
+api.add_resource(Main, '/')
+api.add_resource(UserApi, '/user')
+api.add_resource(TestcaseApi, '/testcase')
+api.add_resource(TaskApi, '/task')
+api.add_resource(ReportApi, '/report')
+
 
 if __name__ == '__main__':
-    # app.run(host, port, debug, options)
-    # 默认值：host="127.0.0.1", port=5000, debug=False
-    # app.run(host="0.0.0.0", port=5000)
     app.run(debug=True)
